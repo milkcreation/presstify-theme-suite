924ef0490ff6319011093fbaa5b9d4f02ea31bcb
Jordy Manner <jordy.manner@milkcreation.fr>
2020-11-25 15:55:11 +0000

    theme-suite v2.0.x-dev

--- views/partial/article-title/index.php
+++ views/partial/article-title/index.php
@@ -6,12 +6,12 @@
 ?>
 <<?php echo $this->get('tag', 'h1'); ?> <?php echo $this->htmlAttrs(); ?>>
     <?php if ($before = $this->get('before')) : ?>
-        <span class="ArticleTitle--before"><?php echo $before; ?></span>
+        <span class="ArticleTitle-before"><?php echo $before; ?></span>
     <?php endif; ?>
     <?php if ($content = $this->get('content')) : ?>
-        <span class="ArticleTitle--content"><?php echo $content; ?></span>
+        <span class="ArticleTitle-content"><?php echo $content; ?></span>
     <?php endif; ?>
     <?php if ($after = $this->get('after')) : ?>
-        <span class="ArticleTitle--after"><?php echo $after; ?></span>
+        <span class="ArticleTitle-after"><?php echo $after; ?></span>
     <?php endif; ?>
 </<?php echo $this->get('tag', 'h1'); ?>>
